fd4d3b94d780c8c7030076f7047fbcb5f8080117
Heikki88 <heikkip77@gmail.com>
2025-10-21 14:34:29 +0300

    Arvauspelin muutokset

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        String answer = "Emma";
+        String yritys = "";
+        int aKerta = 0;
+
+        while (yritys != answer) {
+
+            System.out.println("Guess my name (type stop to exit)");
+            yritys = in.nextLine();
+            aKerta++;
+
+            if (yritys.equals("stop")) {
+                System.out.println("You guessed " + (aKerta - 1) + " times.");
+                break;
+            }
+
+            if (yritys.equals(answer)) {
+                System.out.println("Congratulations!");
+                System.out.println("You guessed " + aKerta + " times.");
+                break;
+            }
+        }
+        in.close();
+    }
+
+}
